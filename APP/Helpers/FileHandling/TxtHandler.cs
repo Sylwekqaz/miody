60397525996da5b91d8355b95e16b338bfa1db0d
rafalbany <rafal.bany@op.pl>
2015-05-19 16:39:48 +0200

    poprawki txthandlera-wykrywanie podobienstw

--- APP/Helpers/FileHandling/TxtHandler.cs
+++ APP/Helpers/FileHandling/TxtHandler.cs
@@ -14,6 +14,7 @@ namespace APP.Helpers.FileHandling
         Contour LoadTxt(TextReader reader);
     }
 
+
     public class TxtHandler : ITxtHandler
     {
         /// <summary>
@@ -27,33 +28,141 @@ namespace APP.Helpers.FileHandling
         /// Zwraca kontur
         /// </returns>
         public Contour LoadTxt(TextReader reader)
-    {
+        {
             //Pylek numero = 1;
             //Pylek kolorp = KnownColor.ActiveCaption;
             //Pylek nazwo = "rzepakowy";
             //string name;
-       
-            
-            Contour wynikContour = new Contour();
+
+            IEnumerable<Pollen> pylki = Pollen.Values;   
+            string[] line;
+            double podobienstwo = 0;
+            Contour wynikContour = new Contour(0, 0);    //todo- zrobic te wysokosc i szerokosc do konturu
 
             while (reader.Peek() != -1)
-        {
-                 string readLine = reader.ReadLine();
-                 if (readLine != null)
             {
-                     string[] line = readLine.Split(' ');
-                     ContourPoint point = new ContourPoint()
-                     {  // kazda linijka to odpowiednio współrzędna: X Y Typ pyłku ;rozna ilosc spacji
-                         Location = new Point(int.Parse(line[0]), int.Parse(line[1])),
-                         Type =  (Pollen) line[2]     
- 
-                     };
-                     wynikContour.ContourSet.Add(point); 
-                 }
+
+                string readLine = reader.ReadLine();
+
+                line = readLine.Split(' ');
+
+                if (readLine != null)
+                {
+                    if (line.Length > 2)
+                    {
+
+
+                        podobienstwo = (double)MatchFinder(pylki, line[2]).ToList<object>()[1];
+
+                        if (podobienstwo > 0.55)
+                        {
+                            ContourPoint point = new ContourPoint()
+                            {
+                                Location = new Point(int.Parse(line[0]), int.Parse(line[1])),
+                                Type = (Pollen)((string)MatchFinder(pylki, line[2]).ToList<object>()[0])
+
+                            };
+
+                            wynikContour.ContourSet.Add(point);
+                        }
+
+
+                    }
+                }
             }
-           
-            
+
+            reader.Close();
             return wynikContour;
+
+            
+           
         }
+
+        protected IEnumerable<object> MatchFinder(IEnumerable<Pollen> obj, string line)
+        {
+            double max = 0;
+            string podejrzanyTypPylku = " ";
+
+            foreach (Pollen item in obj)
+            {
+
+
+                double odleglosc = item.Name.CompareToString(line);
+
+                if (odleglosc != 1)
+                {
+
+                    if (odleglosc > max)
+                    {
+                        max = odleglosc;
+                        podejrzanyTypPylku = item.Name;
+                    }
+
+
+                }
+                else
+                {
+
+                    max = 1;
+                    podejrzanyTypPylku = item.Name;
+                    break;
+
+                }
+
+            }
+
+            List<object> result = new List<object>();
+            result.Add(podejrzanyTypPylku);
+            result.Add(max);
+            return result;
+
+        }
+
+
     }
+
+    public static class CompareStrings
+    {
+
+        public static double CompareToString(this string source, string target) //metoda Levenshteina: http://social.technet.microsoft.com/wiki/contents/articles/26805.calculating-percentage-similarity-of-two-strings-in-c.aspx
+        {
+            if ((source == null) || (target == null)) return 0;   //są nieporównywalne czyli 0% podobieństwa
+            if ((source.Length == 0) || (target.Length == 0)) return 0;    //są nieporównywalne czyli 0% podobieństwa
+            if (source == target) return 1; //są identyczne- 100% podobieństwa
+
+            int sourceWordCount = source.Length;
+            int targetWordCount = target.Length;
+
+            // Step 1
+            if (sourceWordCount == 0)
+                return targetWordCount;
+
+            if (targetWordCount == 0)
+                return sourceWordCount;
+
+            int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
+
+            // Step 2
+            for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
+            for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
+
+            for (int i = 1; i <= sourceWordCount; i++)
+            {
+                for (int j = 1; j <= targetWordCount; j++)
+                {
+                    // Step 3
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    // Step 4
+                    distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
+                }
+            }
+            double result = (1.0 - ((double)distance[sourceWordCount, targetWordCount] / (double)Math.Max(source.Length, target.Length)));
+
+            return result;
+
+        }
+
+    }
+
 }
