d04030d5361510f2d5c5e03b3914268235d5d9a8
Sylwekqaz <sylwekqaz19@gmail.com>
2015-05-19 20:40:59 +0200

    Poprawki

--- APP/Helpers/Measures/JaccardIndex.cs
+++ APP/Helpers/Measures/JaccardIndex.cs
@@ -1,24 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using APP.Model;
 
 namespace APP.Helpers.Measures
 {
     internal class JaccardIndex : IComparison
     {
+        private MaskGenerator _maskGenerator;
+
+        public JaccardIndex(MaskGenerator maskGenerator)
+        {
+            _maskGenerator = maskGenerator;
+        }
+
         public Result GetResult(Contour a, Contour b)
         {
-            MaskGenerator Z = new MaskGenerator();
-            Mask MaskaA = Z.GenerateMask(a);
-            Mask MaskaB = Z.GenerateMask(b);
-            int mocA = MocZbioru(MaskaA);
-            int mocB = MocZbioru(MaskaB);
-            int mocCzęściWspólnej = MocCzęściWspólnej(MaskaA, MaskaB);
+            Mask maskaA = _maskGenerator.GenerateMask(a);
+            Mask maskaB = _maskGenerator.GenerateMask(b);
+
+            int mocA = MocZbioru(maskaA);
+            int mocB = MocZbioru(maskaB);
+
+            int mocCzęściWspólnej = MocCzęściWspólnej(maskaA, maskaB);
             int mocSumy = mocA + mocB - mocCzęściWspólnej;
-            double wynik = mocCzęściWspólnej/mocSumy;
+
+            double wynik = mocCzęściWspólnej/(double)mocSumy;
             return new Result {Title = "Indeks Jaccarda", D = wynik};
         }
 
@@ -33,7 +38,7 @@ namespace APP.Helpers.Measures
                 {
                     for (int j = 0; j < item1.Value.GetLength(1); j++)
                     {
-                        if (item1.Value[i, j] == true)
+                        if (item1.Value[i, j])
                         {
                             licznik++;
                         }
@@ -51,19 +56,16 @@ namespace APP.Helpers.Measures
 
             foreach (KeyValuePair<Pollen, bool[,]> item1 in A)
             {
-                foreach (KeyValuePair<Pollen, bool[,]> item2 in B)
+                var item2 = B[item1.Key];
+
+
+                for (int i = 0; i < item1.Value.GetLength(0); i++)
                 {
-                    if (item1.Key == item2.Key)
+                    for (int j = 0; j < item1.Value.GetLength(1); j++)
                     {
-                        for (int i = 0; i < item1.Value.GetLength(0); i++)
+                        if (item1.Value[i, j] && item2[i, j])
                         {
-                            for (int j = 0; j < item1.Value.GetLength(1); j++)
-                            {
-                                if (item1.Value[i, j] == item2.Value[i, j] == true)
-                                {
-                                    licznik++;
-                                }
-                            }
+                            licznik++;
                         }
                     }
                 }
