f97381ecf95d352b686a9938c450e3105321d6d9
docsportello <wiktor.suchecki@gmail.com>
2015-05-19 12:39:45 +0200

    jaccard od suchoraba

--- APP/Helpers/Measures/Miara1.cs
+++ APP/Helpers/Measures/Miara1.cs
@@ -3,50 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using APP.Model;
 
 namespace APP.Helpers.Measures
 {
-    class Miara1 
+    class JaccardIndex : IComparison
     {
-        //todo oganrąć ten burdel tutaj
+        public Result GetResult(Contour a, Contour b)
+        {
+            MaskGenerator Z = new MaskGenerator();
+            Mask MaskaA = Z.GenerateMask(a);
+            Mask MaskaB = Z.GenerateMask(b);
+            int mocA = MocZbioru(MaskaA);
+            int mocB = MocZbioru(MaskaB);
+            int mocCzęściWspólnej = MocCzęściWspólnej(MaskaA, MaskaB);
+            int mocSumy = mocA + mocB - mocCzęściWspólnej;
+            double wynik = mocCzęściWspólnej / mocSumy;
+            return new Result { Title = "Indeks Jaccarda", D = wynik };
+        }
+        public int MocZbioru(Mask a)
+        {
+            IReadOnlyDictionary<Pollen, bool[,]> A = a.MaskMap;
 
-            static double miara1(int[,] tab1, int[,] tab2)
+            int licznik = 0;
+            foreach (KeyValuePair<Pollen, bool[,]> item1 in A)
             {
-                int iloczyn = 0;
-                int suma = 0; 
-                double Result = iloczyn / suma; // todo fixme please 
 
-                for (int i = 0; i < tab1.GetLength(0); i++)
+                for (int i = 0; i < item1.Value.GetLength(0); i++)
                 {
-                    for (int j = 0; j < tab1.GetLength(1); j++)
+                    for (int j = 0; j < item1.Value.GetLength(1); j++)
                     {
-                        if (tab1[i, j] != 0 && tab1[i, j] == tab2[i, j])
+                        if (item1.Value[i, j] == true)
                         {
-                            iloczyn++;
-                        }
-                        if ((tab1[i, j] != 0 && tab2[i, j] == 0) || (tab1[i, j] == 0 && tab2[i, j] != 0))
-                        {
-                            suma++;
-                        }
-                        if (tab1[i, j] != 0 && tab2[i, j] != 0 && tab1[i, j] != tab2[i, j])
-                        {
-                            suma += 2;
+                            licznik++;
                         }
                     }
                 }
-                //suma = suma - iloczyn;
-                return Result;
+
             }
-
-
-
-
-
-
-
-
-            
+            return licznik;
         }
+        public int MocCzęściWspólnej(Mask a, Mask b)
+        {
+            IReadOnlyDictionary<Pollen, bool[,]> A = a.MaskMap;
+            IReadOnlyDictionary<Pollen, bool[,]> B = b.MaskMap;
+            int licznik = 0;
+
+            foreach (KeyValuePair<Pollen, bool[,]> item1 in A)
+            {
+                foreach (KeyValuePair<Pollen, bool[,]> item2 in B)
+                {
+                    if (item1.Key == item2.Key)
+                    {
+                        for (int i = 0; i < item1.Value.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < item1.Value.GetLength(1); j++)
+                            {
+                                if (item1.Value[i, j] == item2.Value[i, j] == true)
+                                {
+                                    licznik++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return licznik;
+        }
+
+    }
     }
 
 
