962a089c96e7521b2bf3fbae6da60260aa403684
kamil <Kamil_wit1@wp.pl>
2015-05-31 15:24:38 +0200

    wczytywanie z bitmapy  - tylko miodowych kolorów

---
+++ APP/Helpers/FileHandling/BitmapFromContour.cs
@@ -0,0 +1,24 @@
+﻿using APP.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Helpers.FileHandling
+{
+    class BitmapFromContour
+    {
+        public static Bitmap Result(Contour contour)
+        {
+            Bitmap result = new Bitmap(contour.Height, contour.Width);
+            foreach (var item in contour.ContourSet)
+            {
+                result.SetPixel(item.Location.X, item.Location.Y, item.Type.Color.ToDrawingColor());
+            }
+            return result;
+
+        }
+    }
+}
